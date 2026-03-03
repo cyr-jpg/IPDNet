ac0e9f2138227e63a99dce6af51e73457786827c
cyr <2427941384@qq.com>
2026-03-03 19:02:00 +0800

    Update RepASConv.py

--- ultralytics/nn/modules/RepASConv.py
+++ ultralytics/nn/modules/RepASConv.py
@@ -343,3 +343,152 @@ class RepASConv5x5(nn.Module):
         return self.activation(
             self.branch1(x) + self.branch2(x) + self.branch3(x)
         )
+
+
+class RepASConv7x7(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, deploy=False, use_act=True):
+        super().__init__()
+        self.deploy = deploy
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.use_act = use_act
+        self.activation = nn.SiLU() if use_act else nn.Identity()
+
+        self.identity_valid = (in_channels == out_channels) and (stride == 1)
+
+        if deploy:
+            self.fused_conv = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                stride=stride,
+                padding=0,
+                bias=True
+            )
+        else:
+            self.branch1 = self._build_identity_branch()
+            self.branch2 = self._build_branch2()
+            self.branch3 = self._build_branch3()
+
+    def _build_identity_branch(self):
+        if self.identity_valid:
+            return nn.Sequential(
+                nn.Identity(),
+                nn.BatchNorm2d(self.in_channels, eps=1e-5)
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(self.in_channels, self.out_channels, 1, stride=self.stride, bias=False),
+                nn.BatchNorm2d(self.out_channels, eps=1e-5)
+            )
+
+    def _build_branch2(self):
+        return nn.Sequential(
+            nn.Conv2d(self.in_channels, self.in_channels, 1, stride=self.stride, bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.in_channels, (1, 7), stride=1, padding=(0, 3), bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.in_channels, (7, 1), stride=1, padding=(3, 0), bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.out_channels, 1, stride=1, bias=False),
+            nn.BatchNorm2d(self.out_channels, eps=1e-5)
+        )
+
+    def _build_branch3(self):
+        return nn.Sequential(
+            nn.Conv2d(self.in_channels, self.in_channels, 1, stride=self.stride, bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.in_channels, (7, 1), stride=1, padding=(3, 0), bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.in_channels, (1, 7), stride=1, padding=(0, 3), bias=False),
+            nn.BatchNorm2d(self.in_channels, eps=1e-5),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.out_channels, 1, stride=1, bias=False),
+            nn.BatchNorm2d(self.out_channels, eps=1e-5)
+        )
+
+    def _fuse_conv_bn(self, conv, bn):
+        std = torch.sqrt(bn.running_var + bn.eps)
+        weight = conv.weight * (bn.weight / std).reshape(-1, 1, 1, 1)
+        bias = bn.bias - (bn.running_mean * bn.weight) / std
+        return weight, bias
+
+    def _fuse_identity_branch(self):
+        if self.identity_valid:
+            bn = self.branch1[1]
+            identity_kernel = torch.zeros(
+                self.out_channels, self.in_channels, 1, 1,
+                device=bn.weight.device
+            )
+            for i in range(min(self.in_channels, self.out_channels)):
+                identity_kernel[i, i, 0, 0] = 1.0
+            std = torch.sqrt(bn.running_var + bn.eps)
+            weight = identity_kernel * (bn.weight / std).reshape(-1, 1, 1, 1)
+            bias = bn.bias - (bn.running_mean * bn.weight) / std
+            return weight, bias
+        else:
+            return self._fuse_conv_bn(self.branch1[0], self.branch1[1])
+
+    def _fuse_branch(self, branch):
+        w1, b1 = self._fuse_conv_bn(branch[0], branch[1])
+        w2, b2 = self._fuse_conv_bn(branch[3], branch[4])
+        w3, b3 = self._fuse_conv_bn(branch[6], branch[7])
+        w4, b4 = self._fuse_conv_bn(branch[9], branch[10])
+
+        w12 = F.conv2d(
+            w1.permute(1, 0, 2, 3),
+            w2,
+            padding=(w2.shape[2] // 2, w2.shape[3] // 2)
+        ).permute(1, 0, 2, 3)
+        b12 = (w2 * b1.reshape(1, -1, 1, 1)).sum(dim=(1, 2, 3)) + b2
+
+        w123 = F.conv2d(
+            w12.permute(1, 0, 2, 3),
+            w3,
+            padding=(w3.shape[2] // 2, w3.shape[3] // 2)
+        ).permute(1, 0, 2, 3)
+        b123 = (w3 * b12.reshape(1, -1, 1, 1)).sum(dim=(1, 2, 3)) + b3
+
+        w_fused = F.conv2d(
+            w123.permute(1, 0, 2, 3),
+            w4,
+            padding=0
+        ).permute(1, 0, 2, 3)
+        b_fused = (w4 * b123.reshape(1, -1, 1, 1)).sum(dim=(1, 2, 3)) + b4
+
+        return w_fused, b_fused
+
+    def switch_to_deploy(self):
+        if self.deploy:
+            return
+        self.deploy = True
+
+        w_id, b_id = self._fuse_identity_branch()
+        w2, b2 = self._fuse_branch(self.branch2)
+        w3, b3 = self._fuse_branch(self.branch3)
+
+        self.fused_conv = nn.Conv2d(
+            self.in_channels,
+            self.out_channels,
+            1,
+            stride=self.stride,
+            padding=0,
+            bias=True
+        )
+        self.fused_conv.weight.data = w_id + w2 + w3
+        self.fused_conv.bias.data = b_id + b2 + b3
+
+        del self.branch1, self.branch2, self.branch3
+
+    def forward(self, x):
+        if self.deploy:
+            return self.activation(self.fused_conv(x))
+        return self.activation(
+            self.branch1(x) + self.branch2(x) + self.branch3(x)
+        )
